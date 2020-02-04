cf5747a5ce3403301522ee4c91f55f3274eda09c
Ashley Rushworth <rushworth.ashley@gmail.com>
2020-02-04 22:54:37 +0000

    Add navbar functionality
    Added event listener to enable or disable the difficulty select depending on the selected mode. Submitting the form logs values to the console,

--- Sudoku/wwwroot/js/site.js
+++ Sudoku/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    // Enable or disable the difficulty select depending on the selected mode
+    $("input[name='mode']").click(function () {
+        var modeValue = $("input[name='mode']:checked").val();
+        $("#difficulty").prop("disabled", modeValue === "solve" ? true : false);
+    });
 
-// Write your JavaScript code.
+    // New Sudoku button event listener
+    $("#newSudoku").submit(function (e) {
+        e.preventDefault();
+        var data = $("#newSudoku :input").serializeArray();
+        console.log(data);
+    });
+});
+
+
